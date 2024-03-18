ac2f6ea2e4b89948dc7f35464461b555cf2db604
Jonas Davila <jonas.qa.software@gmail.com>
2024-03-18 13:28:47 -0300

    feat(api): Adicionar arquivo src/api/index.js com a configuração da API

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const router = express.Router();
-const securityMiddleware = require('../middleware/securityMiddleware');
-const transactionService = require('../../services/transactionService');
-const securityConfig = require('../config/security');
+import security from '../../config/security.js'
+import express from 'express'
+import securityMiddleware from '../middleware/securityMiddleware.js'
+import transactionService from '../../services/transactionService.js'
+import securityConfig from '../config/security.js'
 
-const app = express();
+const app = express()
 
 // Configuração do Express e Rotas
-app.use(securityConfig);
-app.use(express.json());
+app.use(securityConfig)
+app.use(express.json())
 
 // Rota para processamento seguro de transações
-router.post('/transacoes', securityMiddleware, (req, res) => {
-    const transactionData = req.body;
+app.post('/transacoes', securityMiddleware, (req, res) => {
+  const transactionData = req.body
 
-    // Verificações de segurança contra SQL Injection e XSS
-    const isSqlInjectionAttempt = transactionService.simulateSqlInjection(transactionData);
-    const isXSSAttempt = transactionService.simulateXSS(transactionData);
+  // Verificações de segurança contra SQL Injection e XSS
+  const isSqlInjectionAttempt =
+    transactionService.simulateSqlInjection(transactionData)
+  const isXSSAttempt = transactionService.simulateXSS(transactionData)
 
-    if (isSqlInjectionAttempt || isXSSAttempt) {
-        res.status(400).json({ error: 'Operação não permitida devido a tentativa de ataque.' });
-    } else {
-        // Lógica de processamento de transação
-        transactionService.processTransaction(transactionData);
+  if (isSqlInjectionAttempt || isXSSAttempt) {
+    res
+      .status(400)
+      .json({ error: 'Operação não permitida devido a tentativa de ataque.' })
+  } else {
+    // Lógica de processamento de transação
+    transactionService.processTransaction(transactionData)
 
-        res.status(200).json({ message: 'Transação processada com segurança.' });
-    }
-});
+    res.status(200).json({ message: 'Transação processada com segurança.' })
+  }
+})
 
 // Rotas
-app.use('/api', router);
+app.use('/api', router)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
 
-module.exports = app;
+export default app
