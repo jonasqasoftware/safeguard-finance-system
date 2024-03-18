dc306b7e965a6048a76c2164bcd7e7032988e108
Jonas Davila <jonas.qa.software@gmail.com>
2024-03-18 13:28:47 -0300

    feat(service): Adicionar arquivo src/services/authenticationService.js com o serviço de autenticação

--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,9 +1,17 @@
-const authConfig = require('../config/auth');
+import { jwtSecret } from '../config/auth.js'
+import jwt from 'jsonwebtoken'
 
+// Função de autenticação de usuário
 function loginUser(username, password) {
-    // Lógica de autenticação
+  // Lógica de autenticação
+  if (username === 'admin' && password === 'senhaSecreta') {
+    // Gera um token JWT com o nome de usuário (pode adicionar mais informações conforme necessário)
+    const token = jwt.sign({ username }, jwtSecret, { expiresIn: '1h' })
+    return token
+  } else {
+    return null // Retornar null em caso de falha na autenticação
+  }
 }
 
-module.exports = {
-    loginUser,
-};
+// Exportar a função de autenticação
+export { loginUser }
