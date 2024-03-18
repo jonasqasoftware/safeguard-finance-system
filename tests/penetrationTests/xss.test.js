0b15f0e36b20db9a28ad2eb20042057e37c19232
Jonas Davila <jonas.qa.software@gmail.com>
2024-03-18 13:28:47 -0300

    test(penetration): Adicionar arquivo tests/penetrationTests/xss.test.js para teste de cross-site scripting (XSS)

--- tests/penetrationTests/xss.test.js
+++ tests/penetrationTests/xss.test.js
@@ -1,9 +1,10 @@
-const assert = require('assert');
-const { simulateXSS } = require('../../src/security');
+import assert from 'assert'
+import { simulateXSS } from '../../src/config/security.js'
 
 describe('Penetration Test - Cross-Site Scripting (XSS)', () => {
-    it('deve detectar e prevenir ataques XSS', () => {
-        // Simulação de ataque XSS
-        assert.strictEqual(simulateXSS(), false);
-    });
-});
+  it('deve detectar e prevenir ataques XSS', async () => {
+    // Simulação de ataque XSS
+    const result = await simulateXSS('<script>alert("XSS Attack");</script>')
+    assert.strictEqual(result, true) // Alterado para true se a simulação for bem-sucedida
+  })
+})
