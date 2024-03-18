c7db2a3dd4c2f514dc3a4a21202770d5d52cfbf7
Jonas Davila <jonas.qa.software@gmail.com>
2024-03-18 13:28:47 -0300

    feat(config): Adicionar arquivo src/config/auth.js com as configurações de autenticação

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,9 +1,14 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+
+// Gere uma chave aleatória para JWT
+const jwtSecret = crypto.randomBytes(32).toString('hex')
+
+// Configure a variável de ambiente para desenvolvimento (não recomendado para produção)
+process.env.JWT_SECRET = jwtSecret
 
 function generateToken(payload) {
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
 }
 
-module.exports = {
-    generateToken,
-};
+export { generateToken, jwtSecret }
